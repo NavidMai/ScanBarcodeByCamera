ead14d87ba3753a9b0394d362a2f078a674ce825
Navid <navid.mai@icloud.com>
2021-02-23 14:14:12 +0800

    Barcode scanner v1.0

--- image_barcode_reader_by_zxing.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: UTF-8 -*-
-import zxing
-
-
-reader = zxing.BarCodeReader()
-barcode = reader.decode("Data/barcode.gif")
-# barcode = reader.decode("Data/qrcode.png")
-print(barcode.parsed)
